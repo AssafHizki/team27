dd04f0232518e447f211d9b95beafe06441aa0ad
Meir <meirtz4@gmail.com>
2020-07-14 21:43:21 +0300

    Logs are now sent to logz.io

--- clients/loggerClient.js
+++ clients/loggerClient.js
@@ -1,20 +1,38 @@
-const bot = require('../clients/telegramClient').getBot();
 const env = require('../environment/environment').env();
+const logger = require('logzio-nodejs').createLogger({
+    token: env.LOGZ_IO_TOKEN,
+    host: 'listener.logz.io',
+    type: 'backend' 
+});
 
 const log = (msg, level) => {
     try {
-        const stringMessage = JSON.stringify(msg)
+        // const stringMessage = JSON.stringify(msg)
+        var obj = { 
+            message: msg, 
+            level: level,
+            env: env.ENV_NAME,
+        };
         if (level=='ERROR') {
             console.error(msg)
-            bot.sendMessage(env.LOG_DEST, `${level}\n${stringMessage}`);
+            logger.log(obj);
         } else if (level=='WARNING') {
             console.warn(msg)
-            bot.sendMessage(env.LOG_DEST, `${level}\n${stringMessage}`);
+            logger.log(obj);
+        } else if (level=='INFO') {
+            console.log(msg)
+            logger.log(obj);
         } else if (level=='DEBUG') {
             console.log(msg)
         } else {
-            console.log(msg)
-            // bot.sendMessage(env.LOG_DEST, `${level}\n${stringMessage}`);
+            message =  `UNKNOWN LOG LEVEL! level = ${level}. message = ${JSON.stringify(msg)}`
+            var obj = { 
+                message: message, 
+                level: 'ERROR',
+                env: env.ENV_NAME,
+            };
+            console.error(message)
+            logger.log(obj);
         }
     } catch (error) {
         console.log(error)
