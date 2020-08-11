4e823128041bf1a5b36511334be30d11d6a1a3a5
Meir <meirtz4@gmail.com>
2020-08-11 23:31:40 +0300

    More tests

--- volunteer/volunteerMsgHandler.test.js
+++ volunteer/volunteerMsgHandler.test.js
@@ -19,4 +19,42 @@ describe('Volunteer message handler', () => {
         })
         await volunteerMsgHandler.newMsg(volunteerId, "Somename", "/register passcode1234")
     });
+
+    it('should not register volunteer with wrong secret', async (done) => {
+        const volunteerId = 2345
+        redis.get.mockImplementation((key) => {
+            if (key.includes('registeredvol')) {
+                return [volunteerId]
+            }
+        });
+        await volunteerMsgHandler.newMsg(volunteerId, "Somename", "/register wrongpasscode1234")
+        expect(redis.set).not.toHaveBeenCalled();
+        done();
+    });
+
+    it('should return registered volunteers to a registered volunteer', async (done) => {
+        const volunteerId = 3456
+        redis.get.mockImplementation((key) => {
+            if (key.includes('registeredvol')) {
+                return [volunteerId]
+            } else if (key.includes(volunteerId)) {
+                done();
+            }
+        });
+        await volunteerMsgHandler.newMsg(volunteerId, "Somename", "/get_registered")
+    });
+
+    // it('should not return registered volunteers to a not registered volunteer', async (done) => {
+    //     const volunteerId = 3456
+    //     redis.get.mockImplementation((key) => {
+    //         if (key.includes('registeredvol')) {
+    //             return []
+    //         } else if (key.includes(volunteerId)) {
+    //             // expect(true).toBe(false)
+    //             console.log("dasada")
+    //         }
+    //     });
+    //     await volunteerMsgHandler.newMsg(volunteerId, "Somename", "/get_registered")
+    //     done();
+    // });
 })
