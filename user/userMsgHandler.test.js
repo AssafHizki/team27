4e1162fb7466e01f5faaeb40664ab3d25e01af1b
Meir <meirtz4@gmail.com>
2020-08-30 17:30:18 +0300

    Tests names for User message handler

--- user/userMsgHandler.test.js
+++ user/userMsgHandler.test.js
@@ -1,5 +1,34 @@
 describe('User message handler', () => {
-    it.skip('should do something', async (done) => {
-        done();
+
+    it.skip('should send message from user to assigned volunteer', async (done) => {
+        done.fail(new Error('Not implemented'))
+    });
+
+    it.skip('should not send message from user to assigned volunteer since user no exist', async (done) => {
+        done.fail(new Error('Not implemented'))
+    });
+
+    it.skip('should not send message from user to assigned volunteer since user not assigned', async (done) => {
+        done.fail(new Error('Not implemented'))
+    });
+
+    it.skip('should start new conversation', async (done) => {
+        done.fail(new Error('Not implemented'))
+    });
+
+    it.skip('should fail to start new conversation since user already exist', async (done) => {
+        done.fail(new Error('Not implemented'))
+    });
+
+    it.skip('should end existing conversation', async (done) => {
+        done.fail(new Error('Not implemented'))
+    });
+
+    it.skip('should fail to end existing conversation since user not exist', async (done) => {
+        done.fail(new Error('Not implemented'))
+    });
+    
+    it.skip('should fail to end existing conversation since user not assigned', async (done) => {
+        done.fail(new Error('Not implemented'))
     });
 })
